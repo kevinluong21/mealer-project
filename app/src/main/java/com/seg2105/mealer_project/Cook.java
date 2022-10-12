5407b917f74de07d5b8f1bc49459e709a98b2fde
RyanFG <rfros048@uottawa.ca>
2022-10-12 15:41:57 -0400

    added attributes to cook class

--- app/src/main/java/com/seg2105/mealer_project/Cook.java
+++ app/src/main/java/com/seg2105/mealer_project/Cook.java
@@ -2,4 +2,11 @@ package com.seg2105.mealer_project;
 
 public class Cook extends Person{
 
+    private String description;
+    private String address;
+    private String voidCheque;
+
+    private int soldMeals;
+    private int customerRating;
+
 }
