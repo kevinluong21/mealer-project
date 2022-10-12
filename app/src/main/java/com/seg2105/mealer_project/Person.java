505dfbc1dd7f9fb6eaa2ce4f86ba5a25bb3402b5
Kevin Luong <kluon030@uottawa.ca>
2022-10-12 15:00:36 -0400

    added abstract Person class

---
+++ app/src/main/java/com/seg2105/mealer_project/Person.java
@@ -0,0 +1,12 @@
+package com.seg2105.mealer_project;
+
+public abstract class Person {
+    private String firstName;
+    private String lastName;
+    private String emailAddress;
+    private String accountPassword;
+    private String address;
+    private String profilePhoto; //source of the photo
+    private String accountStatus; //online, offline
+
+}
