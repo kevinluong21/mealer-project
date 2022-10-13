e5a041f98f06bd8d08595f366873e57268a5be77
RyanFG <rfros048@uottawa.ca>
2022-10-12 21:31:13 -0400

    cook class modifications

--- app/src/main/java/com/seg2105/mealer_project/Cook.java
+++ app/src/main/java/com/seg2105/mealer_project/Cook.java
@@ -1,5 +1,8 @@
 package com.seg2105.mealer_project;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Cook extends Person{
 
     private String description;
@@ -8,5 +11,26 @@ public class Cook extends Person{
 
     private int soldMeals;
     private int customerRating;
+
+    private LinkedList <Meal> meals;
+    private LinkedList <Meal> offeredMeals;
+    private Queue <MealRequest> purchaseRequest;
+
+
+    public Cook (String firstName, String lastName, String emailAddress, String accountPassword, String description, String address, String voidCheque){
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailAddress = emailAddress;
+        this.accountPassword = accountPassword;
+        this.address = address;
+        this.description = description;
+        this.voidCheque = voidCheque;
+
+        this.soldMeals = 0;
+        this.customerRating = 0;
+    }
+
+
 
 }
