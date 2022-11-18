97a847b7c06879062173e3dd0bd579f5ddeae42a
RyanFG <rfros048@uottawa.ca>
2022-11-17 19:42:36 -0500

    input verification for adding meals

--- app/src/main/java/com/seg2105/mealer_project/MealActivity.java
+++ app/src/main/java/com/seg2105/mealer_project/MealActivity.java
@@ -3,10 +3,12 @@ package com.seg2105.mealer_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
@@ -29,12 +31,13 @@ public class MealActivity extends AppCompatActivity implements Serializable {
     EditText editTextAllergens;
     EditText editTextPrice;
     EditText editTextDescription;
+    TextView mealErrorMessage;
     CheckBox checkBoxAddToMenu;
     Button btnBack;
 
     DatabaseReference users = MainActivity.getUsers(); //get user database from MainActivity
 
-    Button btnAddMeal;
+    Button btnAddMealCompleted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,8 +52,9 @@ public class MealActivity extends AppCompatActivity implements Serializable {
         editTextAllergens = (EditText) findViewById(R.id.editTextAllergens);
         editTextPrice = (EditText) findViewById(R.id.editTextPrice);
         editTextDescription = (EditText) findViewById(R.id.editTextDescription);
+        mealErrorMessage = (TextView) findViewById(R.id.mealErrorMessage);
 
-        btnAddMeal = (Button) findViewById(R.id.btnAddMealCompleted);
+        btnAddMealCompleted = (Button) findViewById(R.id.btnAddMealCompleted);
 
 
 
@@ -70,18 +74,21 @@ public class MealActivity extends AppCompatActivity implements Serializable {
 
         users.child(currentCook.getEmailAddress()).child("meals").child(check.getName()).setValue(check);
         */
-        btnAddMeal.setOnClickListener(new View.OnClickListener(){
+        btnAddMealCompleted.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View view) {
-                createMeal(view);
-                Toast.makeText(getApplicationContext(), "Meal added", Toast.LENGTH_LONG).show();
-                clearFields();
+                switch (view.getId()){
+                    case R.id.btnAddMealCompleted:
+                        createMeal(view);
+                        break;
+                }
             }
         });
 
 
     }
+
 
 
     public void createMeal(View v){
@@ -94,13 +101,27 @@ public class MealActivity extends AppCompatActivity implements Serializable {
         String listOfIngredients = editTextIngredients.getText().toString().trim().toLowerCase();
         String listOfAllergens = editTextAllergens.getText().toString().trim().toLowerCase();
         String priceString = editTextPrice.getText().toString().trim().toLowerCase();
-        Double price = Double.parseDouble(priceString);
-        String description = editTextDescription.getText().toString().trim().toLowerCase();
-        boolean isInMenu = checkBoxAddToMenu.isChecked();
+        if (checkPrice(priceString) == true) {
+            Double price = Double.parseDouble(priceString);
+            String description = editTextDescription.getText().toString().trim().toLowerCase();
+            boolean isInMenu = checkBoxAddToMenu.isChecked();
 
-        Meal newMeal = new Meal(mealName, mealType, cuisineType, listOfIngredients, listOfAllergens, price, description, isInMenu);
+            mealErrorMessage.setText("");
 
-        users.child(currentCook.getEmailAddress()).child("meals").child(newMeal.getName()).setValue(newMeal);
+            if (!TextUtils.isEmpty(mealName) && !TextUtils.isEmpty(mealType) && !TextUtils.isEmpty(cuisineType) && !TextUtils.isEmpty(listOfIngredients)
+                    && !TextUtils.isEmpty(listOfAllergens) && !TextUtils.isEmpty(priceString) && !TextUtils.isEmpty(description)) {
+                if(inputValidation(mealType,cuisineType,priceString) == true){
+
+                    Meal newMeal = new Meal(mealName, mealType, cuisineType, listOfIngredients, listOfAllergens, price, description, isInMenu);
+
+                    users.child(currentCook.getEmailAddress()).child("meals").child(newMeal.getName()).setValue(newMeal);
+                    Toast.makeText(getApplicationContext(), "Meal added", Toast.LENGTH_LONG).show();
+                    clearFields();
+                }
+            } else {
+                mealErrorMessage.setText("All fields must be filled");
+            }
+        }
 
     }
 
@@ -115,6 +136,40 @@ public class MealActivity extends AppCompatActivity implements Serializable {
         checkBoxAddToMenu.setChecked(false);
 
     }
+
+    public boolean inputValidation(String meal, String cuisine, String price){
+        if(checkMealType(meal) == true){
+            if(checkCuisineType(cuisine) == true){
+                if(checkPrice(price) == true){
+                    return true;
+                }else{return false;}
+            }else{return false;}
+        }else{return false;}
+    }
+    public boolean checkMealType(String type){
+        type = type.toLowerCase();
+        if(type.equals("main dish") || type.equals("side dish") || type.equals("dessert") || type.equals("appetizer")){
+            return true;
+        }else{
+            mealErrorMessage.setText("Meal type must be main dish, side dish, appetizer, or dessert");
+            return false;
+        }
+    }
+    public boolean checkCuisineType(String type){
+        if(type.matches("[a-zA-Z\\-]*")){
+            return true;
+        }
+        mealErrorMessage.setText("Cuisine type must only contain letters a through z");
+        return false;
+    }
+    public boolean checkPrice(String p){
+        if(p.matches("[0-9]*[.][0-9]{2}")){
+            return true;
+        }
+        mealErrorMessage.setText("Price must be formatted like this 1.00");
+        return false;
+    }
+
 
 
 }
